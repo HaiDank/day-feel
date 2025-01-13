9a93f7c51f58cb13d03a2f993702f02c240207ca
HaiDank <132982748+HaiDank@users.noreply.github.com>
2025-01-14 01:47:40 +0700

    added shadcn

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,68 @@
 <script lang="ts">
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcomeFallback from '$lib/images/svelte-welcome.png';
+	import chroma from 'chroma-js';
+	import { onMount } from 'svelte';
+
+	let slider: HTMLInputElement;
+	let display: HTMLDivElement;
+	let display2: HTMLDivElement;
+	let sliderValue: number = $state(50);
+	export const colors = ['#F7E58C', '#EEBB79', '#E2894D', '#9C495D', '#393B57'];
+
+	const scale = chroma.scale(colors).mode('hsl').domain([0, 100]);
+	const gradient = scale.colors(100)
+
+	$effect(() => {
+		slider.style.background = `linear-gradient(to right, ${gradient.join(', ')})`;
+		// display.style.background = `linear-gradient(to right, ${gradient.join(', ')})`;
+		display2.style.backgroundColor = scale(sliderValue).css();
+	});
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<title>Color Calendar</title>
+	<meta name="description" content="rate day's feeling based on color" />
 </svelte:head>
 
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcomeFallback} alt="Welcome" />
-			</picture>
-		</span>
+<section class="flex flex-col items-center justify-center p-96">
+	<input
+		bind:this={slider}
+		bind:value={sliderValue}
+		type="range"
+		min="0"
+		max="100"
+		id="color-slider"
+		class="slider h-4 w-96 appearance-none rounded-full transition-all"
+	/>
 
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
+	<!-- <div id="display" bind:this={display} class="h-8 w-96"></div> -->
+	<div id="display2" bind:this={display2} class="p-5 m-8"></div>
 </section>
 
 <style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
+	.slider::-webkit-slider-thumb {
+		-webkit-appearance: none;
+		appearance: none;
+		width: 25px;
+		height: 25px;
+		border-radius: 50%;
+		border: 2px solid black;
+		background: #fff;
+		cursor: pointer;
+		transition-property: all;
+		transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+		transition-duration: 150ms;
 	}
 
-	h1 {
-		width: 100%;
+	.slider::-webkit-slider-thumb:hover {
+		background: #ddd;
 	}
 
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
+	.slider::-moz-range-thumb {
+		width: 25px;
+		height: 25px;
+		border-radius: 50%;
+		border: 5px solid black;
+		background: #fff;
+		cursor: pointer;
 	}
 </style>
